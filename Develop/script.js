58562fac54ad386c0ef8327bc5d661cebc3d8407
Morgan Franke <morganleighfranke@gmail.com>
2021-01-11 13:59:17 -0700

    variable declarations and psuedo code

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,25 @@
-// Assignment Code
+//variable declaration
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k"
+, "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var specialCharacter = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", ":", ";", "'", "<", "," ">", ".", "?", "/", "\\", "\""];
+
+
+//functions
+
+
+//event listeners
+
+//log -- call function, setup listeners, act on them
+
+
+// Generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +31,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
